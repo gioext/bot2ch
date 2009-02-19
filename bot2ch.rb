3b1910661ccc5ba0b9940cf3140ad3aa48eb42fc
内田 一樹 <kazuki@kazuki-macbook.local>
2009-02-20 00:09:43 +0900

    final

--- bot2ch.rb
+++ bot2ch.rb
@@ -1,5 +1,12 @@
+# Bot2ch
+# Copyright (c) 2009 Kazuki UCHIDA
+# Licensed under the MIT License:
+#   http://www.opensource.org/licenses/mit-license.php
+
 require 'open-uri'
 require 'kconv'
+require 'yaml/store'
+require 'net/http'
 
 module Bot2ch
   class Menu
@@ -7,8 +14,8 @@ module Bot2ch
       @bbsmenu = 'http://menu.2ch.net/bbsmenu.html'
     end
 
-    def get_board(board_name)
-      reg = Regexp.new("href=(.+#{board_name})", Regexp::IGNORECASE)
+    def get_board(subdir)
+      reg = Regexp.new("href=(.+#{subdir})", Regexp::IGNORECASE)
       open(@bbsmenu) do |f|
         f.each do |line|
           return Board.new($1) if line =~ reg
@@ -46,7 +53,7 @@ module Bot2ch
 
     def get_images
       images = []
-      downloaders = [NormalImageDownloader]
+      downloaders = [NormalImageDownloader, ImepitaDownloader]
       open(@dat) do |f|
         lines = f.read.toutf8
         lines.each do |line|
@@ -68,18 +75,30 @@ module Bot2ch
     end
   end
 
-  class NormalImageDownloader
+  class Downloader
+    attr_accessor :uri, :url
+
     def initialize(url)
       @url = url
+      @uri = URI.parse(@url)
     end
 
-    def download(saveTo)
-      puts "download: #{@url}"
-      open(saveTo, 'wb') do |f|
-        open(@url) do |img|
-          f.write img.read
+    def save(res, saveTo)
+      puts "download: #{url}"
+      case res
+      when Net::HTTPSuccess
+        open(saveTo, 'wb') do |f|
+          f.write res.body
         end
       end
+    end
+  end
+
+  class NormalImageDownloader < Downloader
+    def download(saveTo)
+      http = Net::HTTP.new(uri.host, 80)
+      res = http.get(uri.path)
+      save(res, saveTo)
     end
 
     def self.match(url)
@@ -87,24 +106,41 @@ module Bot2ch
     end
   end
 
+  class ImepitaDownloader < Downloader
+    def download(saveTo)
+      http = Net::HTTP.new(uri.host, 80)
+      headers = {'Referer' => url}
+      res = http.get("/image#{uri.path}", headers)
+      save(res, saveTo)
+    end
+
+    def self.match(url)
+      url =~ /\/\/imepita.jp\/\d+\/\d+/i
+    end
+  end
+
   class App
-    def execute(board)
-      image_root_dir = File.dirname(__FILE__) + '/images'
+    def execute(subdir)
+      root_dir = File.dirname(__FILE__)
+      image_dir = "#{root_dir}/images"
+      db = YAML::Store.new("#{root_dir}/log/thread.db")
       menu = Menu.new
-      board = menu.get_board(board)
+      board = menu.get_board(subdir)
       threads = board.get_threads
       puts "total: #{threads.length} threads"
       threads.each do |thread|
-        images = thread.get_images
+        images = thread.get_images rescue next
         next if images.empty?
-        parent_dir = "#{image_root_dir}/#{thread.dat_no}" 
+        parent_dir = "#{image_dir}/#{thread.dat_no}" 
         Dir.mkdir(parent_dir) unless File.exists?(parent_dir)
         puts "#{thread.title}: #{images.length} pics"
-        # restore
+        downloaded = db.transaction { db[thread.dat_no] } || 0
         images.each_with_index do |image, index|
+          next if index < downloaded
           image.download("#{parent_dir}/#{index}.jpg") rescue next
+          sleep(0.2)
         end
-        # dump
+        db.transaction { db[thread.dat_no] = images.length }
       end
     end
   end
